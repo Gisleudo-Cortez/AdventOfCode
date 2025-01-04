fcbef04bb580dc7459345d7f7e86df0e06214c3c
Gisleudo <gisleudo.cmf@gmail.com>
2025-01-04 18:01:37 -0300

    Add solutions for part 2 day 9 in Python

--- 2015/9/python/main.py
+++ 2015/9/python/main.py
@@ -17,6 +17,7 @@ def parse_input(data: List[str]) -> Dict[str, Dict[str, int]]:
         nodes[destination][origin] = cost #  the cost form dest to origin is the same as the oposite
     return nodes
 
+# Part 1
 def shortest_hamiltonian_path(nodes: Dict[str, Dict[str, int]]) -> Tuple[list[str], int]:
     shortest_distance = float("inf")
     shortest_route = []
@@ -43,11 +44,37 @@ def shortest_hamiltonian_path(nodes: Dict[str, Dict[str, int]]) -> Tuple[list[st
     
     return shortest_route, shortest_distance
 
+def longest_hamiltonian_path(nodes: Dict[str, Dict[str, int]]) -> Tuple[list[str], int]:
+    longest_distance = float("-inf")
+    longest_route = []
+    cities = list(nodes.keys())
+    
+    
+    for route in permutations(cities):
+        current_distance = 0
+        for i in range(len(route) - 1):
+            city = route[i]
+            next_city = route[i+1]
+
+            if city in nodes and next_city in nodes[city]:
+                current_distance += nodes[city][next_city]
+            elif next_city in nodes and city in nodes[next_city]:
+                current_distance += nodes[next_city][city]
+            else:
+                # Just in case there is no node relating to the city
+                print(f"Error, no distance from {city} to {next_city}")
+                break
+        if current_distance > longest_distance:
+            longest_distance = current_distance
+            longest_route = route
+    
+    return longest_route, longest_distance
+
 
 def main() -> None:
     data = Path("../input.txt").read_text().strip().splitlines()
     example = Path("../example.txt").read_text().strip().splitlines()
-
+    print("="*25,"Part 1","="*25)
     nodes = parse_input(data)
     route, distance = shortest_hamiltonian_path(nodes)
     print(f"The Shortest route in the data is {" -> ".join(route)}. with the distance of {distance}")
@@ -55,6 +82,15 @@ def main() -> None:
     nodes = parse_input(example)
     route, distance = shortest_hamiltonian_path(nodes)
     print(f"The Shortest route in the example is {" -> ".join(route)}. with the distance of {distance}")
+
+    print("="*25,"Part 2","="*25)
+    nodes = parse_input(data)
+    route, distance = longest_hamiltonian_path(nodes)
+    print(f"The Longest route in the data is {" -> ".join(route)}. with the distance of {distance}")
+
+    nodes = parse_input(example)
+    route, distance = longest_hamiltonian_path(nodes)
+    print(f"The Longest route in the example is {" -> ".join(route)}. with the distance of {distance}")
 
 if __name__ == "__main__":
     main()
